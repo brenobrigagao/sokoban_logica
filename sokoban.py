ea10324e0f2918602105dcd48f9155cd8af397e5
Ian <ianfelippo2@gmail.com>
2025-06-19 18:15:12 -0300

    Prototipo

--- sokoban.py
+++ sokoban.py
@@ -1,9 +1,4 @@
 from z3 import *
 
-def inicializar(n,m,T):
-    P = [[[Bool(f"P_{i}_{j}_{t}") for t in range(T+1)] for j in range(m)] for i in range(n)]
-    C = [[[Bool(f"C_{i}_{j}_{t}") for t in range(T+1)] for j in range(m)] for i in range(n)]
-    M = [[Bool(f"M_{i}_{j}") for j in range(m)] for i in range(n)]
-    W = [[Bool(f"W_{i}_{j}") for j in range(m)] for i in range(n)]
-    return Solver(), P, C, M, W
+# Input 
 
