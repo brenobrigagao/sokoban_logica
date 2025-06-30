0dd7f73e6c68c0714b03ff91735aeff15220075c
Ian <ianfelippo2@gmail.com>
2025-06-30 20:59:28 -0300

    .

--- examples.py
+++ examples.py
@@ -4,7 +4,7 @@ from maps import maps
 from utils import imprimir_solucao
 
 def example(number,turnos=50):
-    print(f"=== Example {number} ===")
+    print(f"=== Example {number+1} ===")
     imprimir_solucao(solve(maps[number],turnos))
 
 def examples(turnos=50):
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     elif len(argv) == 2:
         examples(turnos=int(argv[1]))
     elif len(argv) == 3:
-        example(int(argv[1]),int(argv[2]))
+        example(int(argv[2]),int(argv[1]))
     else:
         print("Uso: python examples.py [turnos_maximos] [numero_exemplo]")
